12cf18a8f1372ee47ccc65f4e09f93ea05232821
nb-devops <navin.budgam@gmail.com>
2016-06-13 14:41:51 +0100

    chef-node

---
+++ learn-chef/.chef/knife.rb
@@ -0,0 +1,12 @@
+# See http://docs.chef.io/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "nb"
+client_key               "#{current_dir}/nb.pem"
+validation_client_name   "nbudagam-validator"
+validation_key           "#{current_dir}/nbudagam-validator.pem"
+chef_server_url          "https://api.chef.io/organizations/nbudagam"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+
