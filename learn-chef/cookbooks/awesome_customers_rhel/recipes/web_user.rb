b39ab15ba069d6c2b539f2f5ff25003772675ab2
nb-devops <navin.budgam@gmail.com>
2016-06-13 16:17:15 +0100

    web-user recipe

--- learn-chef/cookbooks/awesome_customers_rhel/recipes/web_user.rb
+++ learn-chef/cookbooks/awesome_customers_rhel/recipes/web_user.rb
@@ -3,3 +3,11 @@
 # Recipe:: web_user
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+group node['awesome_customers_rhel']['group']
+
+user node['awesome_customers_rhel']['user'] do
+	group node['awesome_customers_rhel']['group']
+	system true
+	shell '/bin/bash/'
+end
